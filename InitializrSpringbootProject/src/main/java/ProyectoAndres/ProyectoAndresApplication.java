8ddfc064a3cb066d33db0093a3ff52697507d78e
Sebscr <scampos60167@ufide.ac.cr>
2025-02-05 00:38:39 -0600

    Practica
    Ocho paginas de html variadas con sus respectivos archivos css

---
+++ InitializrSpringbootProject/src/main/java/ProyectoAndres/ProyectoAndresApplication.java
@@ -0,0 +1,11 @@
+package ProyectoAndres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAndresApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoAndresApplication.class, args);
+    }
+}
---
+++ InitializrSpringbootProject/src/test/java/ProyectoAndres/ProyectoAndres/ProyectoAndresApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoAndres.ProyectoAndres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAndresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
